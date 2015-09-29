707f9c1b68edc112ebb5e4f0e1044ebb60cebf61
Markus Hanses <me@markushanses.de>
2015-09-29 19:41:27 +0200

    Simple JUnit test added.

--- src/main/java/junit/code/week/FizzBuzz.java
+++ src/main/java/junit/code/week/FizzBuzz.java
@@ -2,4 +2,8 @@ package junit.code.week;
 
 public class FizzBuzz {
 
+    public String say(int number) {
+        return String.valueOf(number);
+    }
+
 }
--- src/test/java/junit/code/week/FizzBuzzTest.java
+++ src/test/java/junit/code/week/FizzBuzzTest.java
@@ -2,11 +2,17 @@ package junit.code.week;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzTest {
 
     @Test
-    public void test() throws Exception {
+    public void whenOneGivenThanOneIsReturned() {
 
+        FizzBuzz subject = new FizzBuzz();
 
+        String result = subject.say(1);
+
+        assertEquals("1", result);
     }
 }
